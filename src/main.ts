a1b021bb931e6a7513ddadadc1b9b9a737825202
Plamenpowa <pkostadinov72@gmail.com>
2023-04-06 15:56:19 +0300

    feat{Quasar,notify}

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,27 @@ import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
+import { Quasar, Notify } from "quasar";
 
+// Import icon libraries
+import "@quasar/extras/material-icons/material-icons.css";
+
+// Import Quasar css
+import "quasar/dist/quasar.css";
 import "./assets/main.css";
 
 const app = createApp(App);
 const pinia = createPinia();
+app.use(Quasar, {
+  plugins: {
+    Notify,
+  },
+  config: {
+    notify: {
+      /* look at QuasarConfOptions from the API card */
+    },
+  },
+});
 app.use(pinia);
 app.use(router);
 
